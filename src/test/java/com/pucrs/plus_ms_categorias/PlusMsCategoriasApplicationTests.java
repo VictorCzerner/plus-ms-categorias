3edb7334a3ee62a491b5178dee8588a85fe08346
gazambuja <gazambuja@grupopanvel.com.br>
2026-06-22 13:55:10 -0300

    Adição spring ao repositório do GitHub.

---
+++ src/test/java/com/pucrs/plus_ms_categorias/PlusMsCategoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucrs.plus_ms_categorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlusMsCategoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
